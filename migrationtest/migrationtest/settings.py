44fe7cabb52e89a7e2b985824512ab09da884e46
Matthias Dellweg <mdellweg@redhat.com>
2021-05-07 12:24:00 +0200

    Add remove_child_model app

--- migrationtest/migrationtest/settings.py
+++ migrationtest/migrationtest/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'remove_child_model',
 ]
 
 MIDDLEWARE = [
---
+++ migrationtest/remove_child_model/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RemoveChildModelConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'remove_child_model'
